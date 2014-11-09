fe088bac0d2df0ac946010fed5f71c26362ad20b
Alain Cajuste <cajuste.alain@gmail.com>
2014-11-09 15:24:03 +0100

    first commit

---
+++ angular-scroll-top.js
@@ -0,0 +1,27 @@
+(function(window, angular, undefined) {
+    'use strict';
+    var ngngScrollTopModule = angular.module('ngScrollTop', ['ng']);
+    ngngScrollTopModule.directive('ngScrollTop', function() {
+        return {
+            restrict: 'AE',
+            replace: false,
+            template: '<div class="scroll-top" id="scroll-top" ng-class="scrollTopColor" >' + '<a id="scroll-link" class="scroll-link" title="Revenir en haut de page">' + '<i class="fa fa-arrow-up scroll-icon"></i>' + '</a>' + '</div>',
+            link: function(scope, $elm, attrs) {
+                var _window = angular.element(window);
+                _window.on('scroll', function() {
+                    var posScroll = _window.scrollTop();
+                    if (posScroll >= 300) {
+                        $elm.fadeIn(600);
+                    } else {
+                        $elm.fadeOut(600);
+                    }
+                });
+                $elm.on('click', function() {
+                    $("html, body").animate({
+                        scrollTop: 0
+                    }, "slow");
+                });
+            }
+        }
+    });
+})(window, window.angular);
